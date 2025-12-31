f1b067c664c9414d1cd857bdb006b045836044d1
Anmol Sharma <anmol.molu22@gmail.com>
2026-01-01 03:59:59 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -28,13 +28,44 @@ app.get('/user', (req, res) => {
     })
 })
 
+// POST API Call
 app.post('/user', (req, res) => {
     res.send("Data successfully saved to the DB")
 })
 
+// DELETE API Call
 app.delete('/user', (req, res) => {
     res.send("User deleted successfully")
 })
+
+
+// ! Advanced routing
+app.get('/ab?c', (req, res) => {
+    res.send("It means the 'b' in the route handler is optional which means if we make a call like 'ac' it will also work");
+});
+
+app.get('/ab+c', (req, res) => {
+    res.send("It means we can write anything after 'b' while making the call but it should always end with 'c' eg.- '/ab dfveruveue c' ");
+});
+
+app.get('/ab*cd', (req, res) => {
+    res.send("It means we can add anything between ab and cd like abANMOLcd, the call will work")
+})
+
+app.get('/a(bc)+d', (req, res) => {
+    res.send("It means we can also group these also")
+})
+
+app.get(/a/, (req, res) => {
+    res.send("This is regex which means if anywhere in the url 'a' appears it will work")
+})
+
+app.get(/.*fly$/, (req, res) => {
+    res.send("This is a complex regex it means if anything ends with a 'fly' it will work")
+})
+
+
+
 
 
 app.listen(3000, () => {
