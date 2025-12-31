a7cfa5eb2e443907793162519cdfb15f2f9a5dee
Anmol Sharma <anmol.molu22@gmail.com>
2026-01-01 03:02:12 +0530

    Running simple request routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("starting the server")
+const express = require("express");
+
+const app = express();
+
+app.use('/test', (req, res) => {
+    res.send("Hello from test")
+})
+
+app.use('/', (req, res) => {
+    res.send("Hello from /")
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000")
+})
