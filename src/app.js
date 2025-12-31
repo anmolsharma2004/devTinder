bbecd0953bd1faf20a97d6515607621f66c20296
Anmol Sharma <anmol.molu22@gmail.com>
2026-01-01 03:36:32 +0530

    Exploring GET, POST, DELETE API Calls

--- src/app.js
+++ src/app.js
@@ -2,14 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.use('/test', (req, res) => {
-    res.send("Hello from test")
+// ! routes order matter a lot
+// app.use('/test', (req, res) => {
+//     res.send("Hello from test")
+// })
+
+// app.use('/api', (req, res) => {
+//     res.send("Hello from api")
+// });
+
+// * This will match all the HTTP Method(get, oost, put etc.) API calls to /hello
+app.use('/hello', (req, res) => {
+    res.send("Hello Hello hello");
+});
+
+// app.use('/', (req, res) => {
+//     res.send("Hello from /")
+// })
+
+// * This will specifically match only the GET HTTP Method API calls to /user 
+app.get('/user', (req, res) => {
+    res.send({
+        "firstName" : "Anmol",
+        "lastName" : "Sharma"
+    })
 })
 
-app.use('/', (req, res) => {
-    res.send("Hello from /")
+app.post('/user', (req, res) => {
+    res.send("Data successfully saved to the DB")
 })
 
+app.delete('/user', (req, res) => {
+    res.send("User deleted successfully")
+})
 
 
 app.listen(3000, () => {
