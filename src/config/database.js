dc45dacfae7f4be8d2382defa5ccf967480ca7ec
Anmol Sharma <anmol.molu22@gmail.com>
2026-01-03 15:12:20 +0530

    auTH

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://anmolmolu22:Anmol2004@nodedevelopment.datlfyx.mongodb.net/devT"
+        "/"
     )
 }
 
