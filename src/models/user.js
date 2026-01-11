b53d29b11b1d753f37db87895dd117b1a5fc117e
Anmol Sharma <anmol.molu22@gmail.com>
2026-01-12 03:07:20 +0530

    Timestamps

--- src/models/user.js
+++ src/models/user.js
@@ -2,39 +2,43 @@ const mongoose = require("mongoose")
 const { isLowercase } = require("validator")
 
 const userSchema = new mongoose.Schema({
-    firstName : {
-        type : String,
-        minLength : 4,
-        maxLength : 10
+    firstName: {
+        type: String,
+        minLength: 4,
+        maxLength: 10
     },
-    lastName : {
-        type : String
+    lastName: {
+        type: String
     },
-    emailID : {
-        type : String,
-        lowercase : true,
-        trim : true,
-        required : true,
-        unique : true
+    emailID: {
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true,
+        unique: true
     },
-    password : {
-        type : String,
-        required : true
+    password: {
+        type: String,
+        required: true
     },
-    age : {
-        type : Number,
-        min : 18,
-        max : 40
+    age: {
+        type: Number,
+        min: 18,
+        max: 40
     },
-    gender : {
-        type : String,
-        validate(value){
-            if (! ["male", "female"].includes(value)) {
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female"].includes(value)) {
                 throw new Error("Gender is not valid")
             }
         }
     }
-})
+}, 
+{
+    timestamps : true
+}
+)
 
 const UserModel = mongoose.model("user", userSchema)
 module.exports = UserModel
